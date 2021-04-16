d81a5fe48d6f1a5b2ba7195166de880e39065253
Sruteesh Kumar Paramata <psurya1967@iitkgp.ac.in>
2021-04-16 16:23:31 +0530

    Added advanced argument parsing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 #!/usr/bin/env node 
 //const parseArgs = require('minimist');
 const chalk = require('chalk');
+const { command } = require('commander');
+
 const command_args = [];
-command_args[0] = {command: "tweet",description: "command used for tweeting. Mention message with \"-m\" or \"--m\" and image path with \"-p\" or \"--p\""}
+
+command_args[0] = {command: "tweet",description: "command used for tweeting. Mention message with \"-m\" or \"--m\" and pic path with \"-p\" or \"--p\""}
+
 command_args[1] = {command: "retweet",description: "command used for retweeting. Mention the tweet id with \"-i\" or \"--i\""}
+
 const command_flags = [];
+
 command_flags[0] = {flags: ["--help","--usage","-help","-usage"],description: "Help for using \"twitter\""};
+
 command_flags[1] = {flags: ["--m","-m"],description: "message to be sent as a tweet"};
-command_flags[2] = {flags: ["--p","-p"],description: "location of the image to be sent along with the message"};
+
+command_flags[2] = {flags: ["--p","-p"],description: "location of the pic to be sent along with the message"};
+
+command_flags[3] =  {flags: ["--i","-i"],description: "id of the tweet to be retweeted"};
+
 var arg_arr = process.argv;
+
 function show_usage()
 {
+    /*
+    It shows the usage of this command line tool
+    */
+
     console.log(chalk.green("[Syntax]: twitter [argument] [flag]","\n"));
     console.log(chalk.green("Available options for argument:"));
     for (let i = 0; i < command_args.length; ++i)
@@ -35,6 +51,8 @@ function show_usage()
         console.log(chalk.green(x));
     }
 }
+
+
 function show_tweet_error(str)
 {
     if (str === "tweet")
@@ -46,11 +64,15 @@ function show_tweet_error(str)
         console.log(chalk.red("[ERROR]: Mandatory flag \"-i\" expected with the argument \"retweet\". It specifies id of the tweet to be retweeted"));
     }
 }
+
+
 if (process.argv.length === 2)
 {
     console.log(chalk.red("[ERROR]: Arguments expected with the command \"twitter\""));
     show_usage();
 }
+
+
 else if (process.argv.length === 3)
 {
     if (process.argv[2] === "--help" || process.argv[2] === "-help" || process.argv[2] === "--usage" || process.argv[2] === "-usage")
@@ -67,13 +89,18 @@ else if (process.argv.length === 3)
         show_usage();
     }
 }
+
+
 else if (process.argv.length === 4)
 {
-    if (process.argv[2] !== "tweet" && process.argv[2] !== "retweet")
+
+    if (process.argv[2] !== "tweet" && process.argv[2] !== "retweet" && process.argv[2] !== "--help" && process.argv[2] !== "-help" && process.argv[2] !== "--usage" && process.argv[2] !== "-usage")
     {
         console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[2]+'\"'));
         show_usage();
     }
+
+
     else if (process.argv[2] === "tweet")
     {
         if (process.argv[3] === "-m" || process.argv[3] === "--m")
@@ -86,6 +113,8 @@ else if (process.argv.length === 4)
             console.log(chalk.red("flag \"-m\" is expected with the argument \"tweet\". It specifies the message to be tweeted"));
         }
     }
+
+
     else if (process.argv[2] === "retweet")
     {
         if (process.argv[3] === "-i" || process.argv[3] === "--i")
@@ -98,12 +127,163 @@ else if (process.argv.length === 4)
             console.log(chalk.red("flag \"-i\" is expected with the argument \"retweet\". It specifies the id of the tweet to be retweeted"));
         }
     }
+
+
+    else
+    {
+        console.log(chalk.red("[WARNING]: No arguments required after "+'\"'+process.argv[2]+'\"'));
+        show_usage();
+    }
+
+
 }
 else if (process.argv.length === 5)
 {
-    if (process.argv[2] !== "tweet" && process.argv[2] !== "retweet")
+
+    if (process.argv[2] !== "tweet" && process.argv[2] !== "retweet" && process.argv[2] !== "--help" && process.argv[2] !== "-help" && process.argv[2] !== "--usage" && process.argv[2] !== "-usage")
     {
         console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[2]+'\"'));
         show_usage();
     }
+
+    else if (process.argv[2] === "tweet")
+    {
+        if (process.argv[3] !== "--m" && process.argv[3] !== "-m")
+        {
+            console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[2]+'\"'+" with the argument \"retweet\""));
+            console.log(chalk.red("flag \"-i\" is expected with the argument \"retweet\". It specifies the id of the tweet to be retweeted"));
+        }
+        else if (process.argv[3] === "--m" || process.argv[3] === "-m")
+        {
+
+        }
+    }
+
+    else if (process.argv[2] === "retweet")
+    {
+
+        if (process.argv[3] !== "--i" && process.argv[3] !== "-i")
+        {
+            console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[3]+'\"'+" with the argument \"retweet\""));
+            console.log(chalk.red("flag \"-i\" is expected with the argument \"retweet\". It specifies the id of the tweet to be retweeted"));
+        }
+
+        else if (process.argv[3] === "--i" || process.argv[3] === "-i")
+        {
+
+        }
+    }
+
+    else
+    {
+        console.log(chalk.red("[WARNING]: No arguments required after "+'\"'+process.argv[2]+'\"'));
+        show_usage();
+    }
+}
+
+else if (process.argv.length == 6)
+{
+
+    if (process.argv[2] !== "tweet" && process.argv[2] !== "retweet" && process.argv[2] !== "--help" && process.argv[2] !== "-help" && process.argv[2] !== "--usage" && process.argv[2] !== "-usage")
+    {
+        console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[2]+'\"'));
+        show_usage();
+    }
+
+    else if (process.argv[2] === "tweet")
+    {
+
+        if (process.argv[3] === "--m" || process.argv[3] === "-m")
+        {
+
+            if (process.argv[5] === "--p" || process.argv[5] === "-p")
+            {
+                console.log(chalk.red("[ERROR]: flag \"-p\" specifies the path of the pic. Either provide the path or remove the flag"));
+            }
+            else if (process.argv[5] !== "--p" && process.argv[5] !== "-p")
+            {
+                console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[5]+'\"'+" with the argument \"retweet\""));
+                show_usage();
+            }
+        }
+
+        else if (process.argv[3] !== "--m" && process.argv[3] !== "-m")
+        {
+            console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[2]+'\"'+" with the argument \"retweet\""));
+            console.log(chalk.red("flag \"-i\" is expected with the argument \"retweet\". It specifies the id of the tweet to be retweeted"));
+        }
+    }
+
+    else if (process.argv[2] === "retweet")
+    {
+        if (process.argv[3] === "--i" || process.argv[3] === "-i")
+        {
+            console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[5]+'\"'+" with the argument \"retweet\""));
+            show_usage();
+        }
+        else if (process.argv[3] !== "--i" && process.argv[3] !== "-i")
+        {
+            console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[3]+'\"'+" with the argument \"retweet\""));
+            console.log(chalk.red("flag \"-i\" is expected with the argument \"retweet\". It specifies the id of the tweet to be retweeted"));
+        }
+    }
+    else
+    {
+        console.log(chalk.red("[WARNING]: No arguments required after "+'\"'+process.argv[2]+'\"'));
+        show_usage();
+    }
+}
+
+else if (process.argv.length === 7)
+{
+    if (process.argv[2] !== "tweet" && process.argv[2] !== "retweet" && process.argv[2] !== "--help" && process.argv[2] !== "-help" && process.argv[2] !== "--usage" && process.argv[2] !== "-usage")
+    {
+        console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[2]+'\"'));
+        show_usage();
+    }
+
+    else if (process.argv[2] === "tweet")
+    {
+        if (process.argv[3] === "--m" || process.argv[3] === "-m")
+        {
+            if (process.argv[5] === "--p" || process.argv[5] === "-p")
+            {
+
+            }
+            else if (process.argv[5] !== "--p" && process.argv[5] !== "-p")
+            {
+                console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[5]+'\"'+" with the argument \"retweet\""));
+                show_usage();
+            }
+        }
+
+        else if (process.argv[3] !== "--m" && process.argv[3] !== "-m")
+        {
+            console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[2]+'\"'+" with the argument \"retweet\""));
+            console.log(chalk.red("flag \"-i\" is expected with the argument \"retweet\". It specifies the id of the tweet to be retweeted"));
+        }
+    }
+    
+    else if (process.argv[2] === "retweet")
+    {
+        if (process.argv[3] !== "--i" && process.argv[3] !== "-i")
+        {
+            console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[3]+'\"'+" with the argument \"retweet\""));
+            console.log(chalk.red("flag \"-i\" is expected with the argument \"retweet\". It specifies the id of the tweet to be retweeted"));
+        }
+
+        else if (process.argv[3] === "--i" || process.argv[3] === "-i")
+        {
+            console.log(chalk.red("[ERROR]: Unexpected argument "+'\"'+process.argv[5]+'\"'+" with the argument \"retweet\""));
+            show_usage();
+        }
+
+    }
+
+    else
+    {
+        console.log(chalk.red("[WARNING]: No arguments required after "+'\"'+process.argv[2]+'\"'));
+        show_usage();
+    }
+
 }
